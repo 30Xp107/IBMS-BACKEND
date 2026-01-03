14c77b74fcd80faf877f76707c8810ba6cef077d
30Xp107 <jhanbrent0630@gmail.com>
2026-01-03 14:13:36 +0800

    Stop tracking .env file as per .gitignore

--- src/middleware/error.ts
+++
@@ -1,9 +0,0 @@
-import { NextFunction, Request, Response } from "express"
-
-export const errorHandler = (err: any, req: Request, res: Response, next: NextFunction) => {
-    console.error(err)
-    const status = err.statusCode || 500
-    res.status(status).json({ success: false, message: err.message || 'Server Error' })
-}
-
-
