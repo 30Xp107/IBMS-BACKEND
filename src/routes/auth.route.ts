48b5750e5dcf9ba9e1b2d9e629aeab80a84603bf
30Xp107 <jhanbrent0630@gmail.com>
2026-01-03 14:05:14 +0800

    changes

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -5,7 +5,7 @@ import { isAuthenticated } from '../middleware/auth'
 const authRoute = Router()
 authRoute.post('/register', register)
 authRoute.post('/login', login)
-authRoute.post('/logout', logout)
+authRoute.post('/logout', logout) 
 authRoute.get('/refresh', refreshToken)
 authRoute.get('/me', isAuthenticated, getMe)
 authRoute.put('/update-user/:id', isAuthenticated, updateUser)
